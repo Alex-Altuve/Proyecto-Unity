c5a5f64245012889a51cf37469517a8518fecda3
Alex Altuve <altuvealex6@gmail.com>
2024-10-11 15:04:22 -0400

    Add main menu

---
+++ Game-FallenKingdoms/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void ModoHistoria()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Salir()
+    {
+        /// Estos se pondra cuando se cree el build
+        //Debug.Log("Salir");
+        //Application.Quit();
+
+        /// esto es para sacarlo del modo game
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+    //public void ModoArcade()
+    //{
+    //    SceneManager.LoadScene("Mundo1");
+    //}
+}
