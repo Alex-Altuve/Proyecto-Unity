6690aa3c90a8a765d6ff08d8767d0e56847cb42c
Handleinchain <santiagoandres999@gmail.com>
2024-12-01 15:51:19 -0400

    agregada cant en vida y estamina

--- Game-FallenKingdoms/Assets/Scripts/Player/HealthBar.cs
+++ Game-FallenKingdoms/Assets/Scripts/Player/HealthBar.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class HealthBar : MonoBehaviour
 {
-
     public Slider slider;
+    public Text healthText; // Referencia al componente Text para mostrar la vida
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        UpdateHealthText(health); // Inicializa el texto con el valor máximo
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        UpdateHealthText(health); // Actualiza el texto con el valor actual
     }
-    
+
+    private void UpdateHealthText(int health)
+    {
+        healthText.text = health.ToString(); // Convertir la salud a texto y mostrarla
+    }
 }
--- Game-FallenKingdoms/Assets/Scripts/Player/StaminaBar.cs
+++ Game-FallenKingdoms/Assets/Scripts/Player/StaminaBar.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class StaminaBar : MonoBehaviour
 {
-
     public Slider slider;
-
+    public Text staminaText; // Referencia al componente Text para mostrar la estamina
 
     public void SetMaxStamina(int stamina)
     {
         slider.maxValue = stamina;
         slider.value = stamina;
+        UpdateStaminaText(stamina); // Inicializa el texto con el valor máximo
     }
 
     public void SetStamina(int stamina)
     {
         slider.value = stamina;
+        UpdateStaminaText(stamina); // Actualiza el texto con el valor actual
+    }
+
+    private void UpdateStaminaText(int stamina)
+    {
+        staminaText.text = stamina.ToString(); // Convertir la estamina a texto y mostrarla
     }
 }
