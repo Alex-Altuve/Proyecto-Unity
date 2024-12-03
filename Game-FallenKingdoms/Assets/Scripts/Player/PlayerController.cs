d4427f341ec11d8bf5cdc164c3efbfe614c70e71
Alex Altuve <altuvealex6@gmail.com>
2024-12-02 21:35:53 -0400

    Feat: add new mechanic (roll)

--- Game-FallenKingdoms/Assets/Scripts/Player/PlayerController.cs
+++ Game-FallenKingdoms/Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,30 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float runSpeed = 8f; // Velocidad al correr
+    public float jumpImpulse = 17f;
     Vector2 moveInput;
+    
     Collider2D playerCollider;
     Rigidbody2D playerRigidBody;
+    Rigidbody2D rb;
+    Animator animator;
 
     TouchingDirections touchingDirections;
 
     public bool _isMoving = false;
     private bool canMove = true; // Controlador de movimiento
-
-    public float leftLimit = -15f;  // Límite izquierdo
-    public float rightLimit = 15f;  // Límite derecho
+    private bool isRunning = false; // Controla si está corriendo
+    private bool isRolling = false;
 
     // Variables de salud
     public int maxHealth = 100;
@@ -30,23 +35,31 @@ public class PlayerController : MonoBehaviour
     public int maxStamina = 100;
     public int currentStamina;
     public StaminaBar staminaBar;
+    
+    ///Reduccion de estamina por accion
+    public int staminaCostPerRoll = 10;
     public int staminaCostPerAttack = 10;
     public int staminaCostPerRun = 5;
-    public float staminaRegenRate = 5f;
-    private float lastAttackTime;
     private float staminaReductionTimer = 0.5f;
-
+    public float staminaRegenRate = 5f;
+   
+    // Ataque
+    private float lastAttackTime;
+   
+    //Daño
     [SerializeField]
     private Damageable damageable;
-
+    
+    //Mensaje
     public Text TextGameOver;
 
-    Rigidbody2D rb;
-    Animator animator;
-    public float jumpImpulse = 17f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
 
-    private bool isRunning = false; // Controla si está corriendo
-
+    }
     void Start()
     {
         currentHealth = maxHealth;
@@ -63,6 +76,95 @@ public class PlayerController : MonoBehaviour
         if (damageable == null)
         {
             damageable = GetComponent<Damageable>();
+        }
+
+    }
+
+    void Update()
+    {
+        Die();
+
+        if (!isRunning && Time.time - lastAttackTime >= 0.75f && currentStamina < maxStamina)
+        {
+            GetStamina(5);
+            lastAttackTime = Time.time;
+        }
+
+        isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        isRolling = Input.GetKey(KeyCode.J);
+    }
+
+    private void FixedUpdate()
+    {
+        // Detener el movimiento si no puede moverse
+        if (!canMove || !CanMove)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            return;
+        }
+
+        rb.velocity = new Vector2(moveInput.x * CurrentMoventSpeed, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+
+        ReduccionEstamina_Mecanica();
+
+    }
+
+    //Daño y Estamina
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+
+            if (damageable != null)
+            {
+                damageable.IsAlive = false; // Cambia el estado a no vivo
+            }
+            animator.SetBool(AnimationStrings.canMove, false);
+            GameOver();
+        }
+    }
+
+    void GetStamina(int stamina)
+    {
+        currentStamina += stamina;
+        staminaBar.SetStamina(currentStamina);
+    }
+
+    //Reduccion de estamina 
+    public void ReduccionEstamina_Mecanica()
+    {
+        if (isRunning && currentStamina >= staminaCostPerRun)
+        {
+            staminaReductionTimer -= Time.fixedDeltaTime;
+
+            if (staminaReductionTimer <= 0)
+            {
+                currentStamina -= staminaCostPerRun;
+                staminaBar.SetStamina(currentStamina);
+                staminaReductionTimer = 0.5f; // Reiniciar el temporizador
+            }
+        }
+        else
+        {
+            staminaReductionTimer = 0.5f;
+        }
+    }
+
+    ///Logica movimiento racing and ismoving
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
         }
     }
 
@@ -116,85 +218,10 @@ public class PlayerController : MonoBehaviour
 
     public bool CanMove => animator.GetBool(AnimationStrings.canMove);
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        touchingDirections = GetComponent<TouchingDirections>();
-    }
-
-    void Update()
-    {
-        Die();
-
-        if (!isRunning && Time.time - lastAttackTime >= 0.75f && currentStamina < maxStamina)
-        {
-            GetStamina(5);
-            lastAttackTime = Time.time;
-        }
-
-        isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
-
-        if (currentHealth <= 0)
-        {
-            currentHealth = 0;
-
-            if (damageable != null)
-            {
-                damageable.IsAlive = false; // Cambia el estado a no vivo
-            }
-
-            Debug.Log("Player has died.");
-            animator.SetBool(AnimationStrings.canMove, false);
-            GameOver();
-        }
-    }
-
-    void GetStamina(int stamina)
-    {
-        currentStamina += stamina;
-        staminaBar.SetStamina(currentStamina);
-    }
-
-    private void FixedUpdate()
-    {
-        // Detener el movimiento si no puede moverse
-        if (!canMove || !CanMove)
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            return;
-        }
-
-        rb.velocity = new Vector2(moveInput.x * CurrentMoventSpeed, rb.velocity.y);
-        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
-
-        if (isRunning && currentStamina >= staminaCostPerRun)
-        {
-            staminaReductionTimer -= Time.fixedDeltaTime;
-
-            if (staminaReductionTimer <= 0)
-            {
-                currentStamina -= staminaCostPerRun;
-                staminaBar.SetStamina(currentStamina);
-                staminaReductionTimer = 0.5f; // Reiniciar el temporizador
-            }
-        }
-        else
-        {
-            staminaReductionTimer = 0.5f;
-        }
-    }
-
+    //Mecanicas ----------------------------------------
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-
         if (damageable.IsAlive)
         {
             IsMoving = moveInput != Vector2.zero;
@@ -203,18 +230,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             IsMoving = false;
-        }
-    }
-
-    private void SetFacingDirection(Vector2 moveInput)
-    {
-        if (moveInput.x > 0 && !IsFacingRight)
-        {
-            IsFacingRight = true;
-        }
-        else if (moveInput.x < 0 && IsFacingRight)
-        {
-            IsFacingRight = false;
         }
     }
 
@@ -228,9 +243,10 @@ public class PlayerController : MonoBehaviour
             lastAttackTime = Time.time;
         }
     }
-
+   
     public void OnJump(InputAction.CallbackContext context)
     {
+       
         // No permitir saltar si el jugador está muerto o no puede moverse
         if (!canMove || !damageable.IsAlive) return;
 
@@ -241,6 +257,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    public void OnRoll(InputAction.CallbackContext context)
+    {
+        // No permitir rodar si el jugador está muerto o no puede moverse
+        if (!canMove || !damageable.IsAlive) return;
+   
+        if (context.started)
+        {
+            animator.SetBool(AnimationStrings.isRolling, true);
+            animator.SetTrigger(AnimationStrings.roll);
+    
+        }
+        else if (context.canceled)
+        {
+            // Si el contexto se cancela, restablece el estado
+            animator.SetBool(AnimationStrings.isRolling, false);
+            
+        }
+    }
+
+
+    /// Mensajes
     public void GameOver()
     {
         TextGameOver.text = "GAME OVER";
@@ -255,11 +293,11 @@ public class PlayerController : MonoBehaviour
 
     public void QuitGame()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
     }
 
     private void Die()
@@ -269,11 +307,7 @@ public class PlayerController : MonoBehaviour
             canMove = false; // Desactivar el movimiento al tocar el layer "Vacio"
             GameOver();
         }
-        if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
-        {
-            // Puedes agregar aquí la lógica para manejar el daño o muerte por enemigos
-            // animator.SetTrigger("dead");
-        }
+ 
     }
 
     
